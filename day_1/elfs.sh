1e1f8cb912eeb3f69326c1b602d0b028c529b311
Ludvik <ludvik.jakub.roubicek@gmail.com>
2022-12-02 00:15:06 +0100

    day2: Elfs 2nd half

--- day_1/elfs.sh
+++ day_1/elfs.sh
@@ -3,6 +3,8 @@
 awk 'BEGIN { file="elfs/elf"++i } !NF {file="elfs/elf"++i; next} {print > file} ' data
 
 MAX_FRUITS=0
+SND_FRUITS=0
+THD_FRUITS=0
 
 for i in ./elfs/*; do
     LN_CNT=$(wc -l <"$i")
@@ -12,8 +14,19 @@ for i in ./elfs/*; do
         ((CURR_FRUITS += $CURR_LN))
     done
     if ((CURR_FRUITS > MAX_FRUITS)); then
+        THD_FRUITS=$SND_FRUITS
+        SND_FRUITS=$MAX_FRUITS
         MAX_FRUITS=$CURR_FRUITS
+    elif ((CURR_FRUITS < MAX_FRUITS && CURR_FRUITS > SND_FRUITS)); then
+        THD_FRUITS=$SND_FRUITS
+        SND_FRUITS=$CURR_FRUITS
+    elif ((CURR_FRUITS < SND_FRUITS && CURR_FRUITS > THD_FRUITS)); then
+        THD_FRUITS=$CURR_FRUITS
     fi
 done
 
 printf '1st: %d\n' $MAX_FRUITS
+printf '2nd: %d\n' $SND_FRUITS
+printf '3rd: %d\n' $THD_FRUITS
+printf '------------\n'
+printf "total: %d\n" $((MAX_FRUITS + SND_FRUITS + THD_FRUITS))
